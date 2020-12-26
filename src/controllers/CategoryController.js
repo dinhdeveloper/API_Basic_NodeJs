a49514eb750a8462540206ec0dd36d5d8a5f2109
dinhdeveloper <dinhtrancntt@gmail.com>
2020-12-26 17:09:44 +0700

    fix upload file image

--- src/controllers/CategoryController.js
+++ src/controllers/CategoryController.js
@@ -32,12 +32,38 @@
 //   uploadFiles,
 // };
 
+const imageModel = require("../models/ImageModel");
+const db = require("../conneted");
+
 function upload(req, res){
   if(req.file.filename){
-      res.status(201).json({
-          mesaage: "Image upload successfully",
-          url: req.file.filename
+    if(res.status(201)){
+      imageModel.name = req.body.name,
+      imageModel.image = req.file.filename
+    }
+
+    var sql =`INSERT INTO category SET `;
+      if(imageModel.name){
+       sql = sql.concat(' ', `name = '${imageModel.name}'`)
+      }
+
+      if(imageModel.image){
+        sql = sql.concat(' ,', `image = '${imageModel.image}'`)
+      }
+
+      db.query(sql, [imageModel], (err, response) => {
+        if (err) throw err;
+        res.json({
+          success: "true",
+          message: "Tạo danh mục thành công",
+        });
       });
+
+
+      // res.status(201).json({
+      //     // mesaage: "Image upload successfully",
+      //     // url: req.file.filename
+      // });
   }else{
       res.status(500).json({
           mesaage: "Something went wrong!"
---
+++ src/models/ImageModel.js
@@ -0,0 +1,7 @@
+
+function ImageModel(name,image){
+    this.name = name,
+    this.image = image
+}
+
+module.exports = new ImageModel
